c7ecd1bc0c8d90d98a486215d7a74f0d1022fddc
Ketan Mehta <ketanmehta4u@gmail.com>
2020-02-16 22:36:39 +0530

    updated the indentation.

--- com/maersk/factorial.py
+++ com/maersk/factorial.py
@@ -1,12 +1,13 @@
 def fact(num):
- result = 1
- if num <=1:
-     return result
- else:
-     while num >= 1:
-        result = result*num
-        num = num-1
-     return result
+    result = 1
+    if num <= 0:
+        return result
+    else:
+        while num >= 1:
+            result = result * num
+            num = num - 1
+        return result
+
 
 n = int(input("Enter the number: "))
 print("The Factorial of", n, "is :", fact(n))
