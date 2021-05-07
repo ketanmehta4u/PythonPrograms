a03e2fa599e3a7bf5fc6f3156f58cd94a45995d1
Ketan Mehta <ketanmehta4u@gmail.com>
2021-05-08 01:18:30 +0530

    updated code for tail recursion

--- com/maersk/FactorialRecursion.py
+++ com/maersk/FactorialRecursion.py
@@ -1,12 +1,12 @@
-# Write a Python Function to find factorial of given number with recursion.
+# Write a Python Function to find factorial of given number using tail recursion.
 
-
-def factorial(n):
-    if n == 0:
-        result = 1
-    else:
-        result = n * factorial(n - 1)
-    return result
-
-
+def factorial(n, a = 1):
+ 
+    if (n == 0):
+        return a
+ 
+    return fact(n - 1, n * a)
+ 
+# Driver program to test
+#  above function
 print("factorial of 5 is: ", factorial(5))
